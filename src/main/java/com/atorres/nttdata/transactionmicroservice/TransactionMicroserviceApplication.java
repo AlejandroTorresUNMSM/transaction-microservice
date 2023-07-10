e008c8aef809ab0c5d5b4fb479511c15f357f3d8
AlejandroTorresUNMSM <alephdmc2575@gmail.com>
2023-07-10 14:33:19 -0500

    commit inicial

---
+++ src/main/java/com/atorres/nttdata/transactionmicroservice/TransactionMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.atorres.nttdata.transactionmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atorres/nttdata/transactionmicroservice/TransactionMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atorres.nttdata.transactionmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
